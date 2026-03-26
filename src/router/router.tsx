b7f0468eb28e61f55b0f2bd50a4aee11408685ca
Igor Muller <igormullersantos@hotmail.com>
2026-03-26 16:13:46 -0300

    Cria rotas e hook para mostrar nome da rota dinamicamente no header do app

---
+++ src/router/router.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import RosaryPage from "../modules/rosary/pages/RosaryPage";
+import LiturgyPage from "../modules/liturgy/pages/LiturgyPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    handle: { name: "Vida Católica" },
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "rosary",
+        handle: { name: "Terço" },
+        element: <RosaryPage />,
+      },
+      {
+        path: "liturgy",
+        handle: { name: "Liturgia Diária" },
+        element: <LiturgyPage />,
+      },
+    ],
+  },
+]);
