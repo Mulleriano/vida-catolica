fa4e7033abaaaa2b7e9a948f6186f3a473a7cc1d
Igor Muller <igormullersantos@hotmail.com>
2026-04-09 16:38:29 -0300

    Primeira estilização da tela de liturgia diária

--- src/modules/liturgy/pages/LiturgyPage.tsx
+++ src/modules/liturgy/pages/LiturgyPage.tsx
@@ -1,7 +1,43 @@
+import React from "react";
 import { useDailyLiturgy } from "../hooks/useDailyLiturgy";
+import { Box, Tabs, Tab } from "@mui/material";
+import getLiturgyHexColor from "../utils/getLiturgyHexColor";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  value: number;
+  index: number;
+}
+
+function TabPanel({ children, value, index }: TabPanelProps) {
+  return (
+    <div hidden={value !== index}>
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
 
 export default function Home() {
   const { liturgy, loading } = useDailyLiturgy();
+  const [value, setValue] = React.useState(0);
+  const tabs = [
+    {
+      label: "1ª Leitura",
+      content: liturgy?.leituras.primeiraLeitura?.[0],
+    },
+    {
+      label: "Salmo",
+      content: liturgy?.leituras.salmo?.[0],
+    },
+    {
+      label: "Evangelho",
+      content: liturgy?.leituras.evangelho?.[0],
+    },
+  ];
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   if (loading || !liturgy) {
     return <p>Carregando liturgia...</p>;
@@ -9,24 +45,44 @@ export default function Home() {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Liturgia do Dia</h1>
+      <button
+        className="w-10 h-10 rounded-full shadow-(--box-shadow) absolute top-4 right-4"
+        style={{ backgroundColor: getLiturgyHexColor(liturgy.cor) }}
+      ></button>
+      <Box sx={{ width: "100%", height: "100%" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          centered
+          className="sticky top-0 bg-(--secondary-color)"
+        >
+          {tabs.map((tab, index) => (
+            <Tab key={index} label={tab.label} />
+          ))}
+        </Tabs>
 
-      <div className="space-y-4">
-        <div>
-          <h2 className="font-semibold">Primeira Leitura</h2>
-          <p>{liturgy.leituras.primeiraLeitura[0]?.texto}</p>
+        <div className="overflow-y-auto">
+          {tabs.map((tab, index) => (
+            <TabPanel key={index} value={value} index={index}>
+              {!tab.content ? (
+                <p>Leitura não disponível</p>
+              ) : "refrao" in tab.content ? (
+                <>
+                  <h5>{tab.content.referencia}</h5>
+                  <h3>{tab.content.refrao}</h3>
+                  <p className="whitespace-pre-line">{tab.content.texto}</p>
+                </>
+              ) : (
+                <>
+                  <h3>{tab.content.titulo}</h3>
+                  <h5>{tab.content.referencia}</h5>
+                  <p className="whitespace-pre-line">{tab.content.texto}</p>
+                </>
+              )}
+            </TabPanel>
+          ))}
         </div>
-
-        <div>
-          <h2 className="font-semibold">Salmo</h2>
-          <p>{liturgy.leituras.salmo[0]?.texto}</p>
-        </div>
-
-        <div>
-          <h2 className="font-semibold">Evangelho</h2>
-          <p>{liturgy.leituras.evangelho[0]?.texto}</p>
-        </div>
-      </div>
+      </Box>
     </div>
   );
 }
